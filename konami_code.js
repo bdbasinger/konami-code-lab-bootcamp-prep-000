5d1bdb10e9389665083f9a411a01f56bb6356d18
Brennan B <bdbasinger777@gmail.com>
2018-07-06 03:20:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,19 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+let index = 0;
 
 function init() {
-  // your code here
+  // Write your JavaScript code inside the init() function
+  document.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
