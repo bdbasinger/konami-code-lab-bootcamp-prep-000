55d28c72cd5248c16fa1b84c4d03a2470fa8678c
Brennan B <bdbasinger777@gmail.com>
2018-07-06 03:27:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -17,6 +17,11 @@ function init() {
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
     
+    
+    
+    
+    
+    
     idx = (codes[idx] === key) ? ++idx : 0
     
     if (idx === codes.length) {
